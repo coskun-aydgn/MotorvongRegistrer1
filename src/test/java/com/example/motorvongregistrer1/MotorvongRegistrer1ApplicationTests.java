adcd1d8a9a3304c57b31a5065d8a34029a51e796
47462 <coskun.aydn@gmail.com>
2024-04-11 23:01:58 +0200

    Initial commit

---
+++ src/test/java/com/example/motorvongregistrer1/MotorvongRegistrer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorvongregistrer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorvongRegistrer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
